41491ae7fe2c9e1d4abe830f6b2aef7945f39d44
brianboshes <brian.boshes@gmail.com>
2013-06-22 22:00:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localvore::Application.config.secret_token = '372e3e0499ceb788ed66ae323402da26fce31596dc11fd9f9b9181e8efdaa08209269228d0b4065fee88c704bacc2b42f21e5c939ac60ac8abdcf55b12b39c2c'
